39a58ef6075561ce87102fefc2b1b16741ffb68c
liutao <liutao@xiwicloud.com>
2026-07-08 21:32:08 +0800

    fix: 1
    Signed-off-by: liutao <liutao@xiwicloud.com>

--- unpackage/dist/dev/mp-weixin/utils/cosConfig.js
+++ unpackage/dist/dev/mp-weixin/utils/cosConfig.js
@@ -1,2 +1,2 @@
-"use strict";const S="cloud2-1258683316",C="ap-shanghai";const _="getCosSts";exports.COS_BUCKET=S;exports.COS_REGION=C;exports.COS_STS_FUNCTION=_;exports.COS_URL_EXPIRES=86400;
+"use strict";const E="AKIDJlLz3DPesHf5TrIdxKUcvHczcEtTxRzT",S="Y8d5MPE5m3Pdo5RoFmIpP9xpjgijndIx",_="cloud2-1258683316",C="ap-shanghai";exports.COS_BUCKET=_;exports.COS_REGION=C;exports.COS_SECRET_ID=E;exports.COS_SECRET_KEY=S;exports.COS_URL_EXPIRES=86400;
 //# sourceMappingURL=../../.sourcemap/mp-weixin/utils/cosConfig.js.map
